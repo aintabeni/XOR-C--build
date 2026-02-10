7f1ca7b157c124349bbec2fb2a56ab87f175734c
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-10 11:18:05 +0300

    Made the boolean variable that stores the XOR value of two variables p and q

--- xor.cpp
+++ xor.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    bool p = 0;
+    bool q = 0;
+    //pXORq Calculation
+    bool pXORq = !p&&q || p&&!q;
+
+    cout << (p ^ q);
+    cout << pXORq;
+
+
 }
